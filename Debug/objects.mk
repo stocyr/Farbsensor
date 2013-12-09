a850f87c6e9c8bf4729a83fad5cd87963a2ab1de
Cyril Stoller <cyril.stoller@gmail.com>
2013-12-09 12:26:58 +0100

    deleted old eclipse project - duplication is NOT working properly...

--- Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lstdc++ -lxynth -lwidget -lwidgetoo -lSDL -ldirect -ldirectfb -lfusion -lts -lz -lm
-
